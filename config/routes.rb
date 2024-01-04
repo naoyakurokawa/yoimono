fc5b9ce05fb610b05aad2eded092610a1e31c42c
naoyakurokawa <kurokawa14@gmail.com>
2024-01-04 23:06:24 +0900

    バージョンダウン

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,21 @@ Rails.application.routes.draw do
   resources :items, only: %i[new create show edit update]
   resources :relationships, only: [:create, :destroy]
   get 'mypage', to: 'users#show_mypage'
+
+  namespace :api, { format: 'json' } do
+    resources :relationships, only: %i[index create destroy]
+  end
 end
+
+
+# constraints: 動的セグメントのURLフォーマットを特定の形式に制限
+## 例: get '/articles/:id' , to: 'articles#show', constraints: { id: /[A-Z]\d{5}/ }
+## match => /articles/A11111, not_match => /articles/111
+
+# asオプションをつけると、名前を指定することができる
+## 例: get 'exit', to: 'sessions#destroy', as: :logout
+## 名前付きヘルパーとしてlogout_pathとlogout_urlが作成
+
+# scope:  URL: 指定のパスにしたい	ファイル構成: 変えたくない
+# namespace  URL: 指定のパスにしたい	ファイル構成: 指定のパスにしたい
+# module  URL: 変えたくない	ファイル構成: 指定のパスにしたい
